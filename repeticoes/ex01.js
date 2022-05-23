4c9138d9a352bfc299207572c5cc39f0de63b5c5
Gustavo <gutoobedin@gmail.com>
2022-05-22 23:06:25 -0300

    Repetições

---
+++ repeticoes/ex01.js
@@ -0,0 +1,26 @@
+
+
+var c = 1
+
+while (c <= 6) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+
+do  {
+    console.log(`Tudo bem? ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
+
