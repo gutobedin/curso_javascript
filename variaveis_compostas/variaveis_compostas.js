82528665b707e0716b75344a1e68f0814cfd9a5f
Gustavo <gutoobedin@gmail.com>
2022-05-28 14:33:13 -0300

    Variaveis Compostas

--- variaveis_compostas/variaveis_compostas.js
+++ variaveis_compostas/variaveis_compostas.js
@@ -1,20 +1,23 @@
 
 let valores = [8, 1, 7, 4, 2, 9]
-valores.sort() 
+valores.sort() // Ordenar valores em ordem crecente
+
+
 /* for(let pos=0; pos < valores.length;pos++){ // quando colocamos .length, nos referimos ao numero de elementos que temos dentro do vetor
     console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
     // a posicao 0 tem o valor 8
 } */
 
 
-for(let pos in valores) {
+for(let pos in valores) { //Para cada posicao na variavel composta, eu vou mostrar a posiçao e o valor valores(pos)
     console.log(`A posicao ${pos} tem o valor ${valores[pos]}`)
 }
 
+// cria um novo elemento dentro do vetor
 // se eu quiser buscar valores dentro do vetor, eu uso a funcao .indexOf()
 // ex
 
-let pos = valores.indexOf(2)
+let pos = valores.indexOf(2) // Ele busca no vetor qual é o valor da posicão 2
 
 if (pos == -1) {
     console.log(`A posicao nao foi encontrada!`)
