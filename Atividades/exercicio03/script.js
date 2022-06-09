7798a92e97eda6f03fb00f9cadd1af10226afd72
Gustavo Bedin <gutoobedin@gmail.com>
2022-06-09 01:26:51 -0300

    Refazendo Tabuada

--- Atividades/exercicio03/script.js
+++ Atividades/exercicio03/script.js
@@ -2,7 +2,6 @@ function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
 
-
     if (num.value.length == 0) {
         window.alert('Por favor digite um numero ')
     } else {
