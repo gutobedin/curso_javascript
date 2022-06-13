2bca2b3cc7e24132599188be78e2d42f56fd106e
Gustavo Bedin <gutoobedin@gmail.com>
2022-06-13 19:36:26 -0300

    Analisando Dados exercicio

--- Atividades/exercicio05/script.js
+++ Atividades/exercicio05/script.js
@@ -22,8 +22,37 @@ function inList(n, a) {
 
 function adicionar() {
     if (isNumero(num.value) && !inList(num.value, array)) {
-        window.alert('Tudo ok')
+       array.push(Number(num.value))
+       result = document.createElement('option')
+       list.appendChild(result)
+       result.text = `Numero ${num.value} adicionado.`
+       res.innerText = ''
+       num.value = ''
+       num.focus();
+       
+
     } else {
         window.alert('[ERRO] O valor é invalido ou ja esta adicionado na lista!')
+    }
+}
+function analisar() {
+    if (array.length == 0) {
+        window.alert('Adicione valores antes de a analisar')
+    } else {
+        res.innerText = `Foi adicionado a lista ${array.length} numeros.
+        `
+        let max = Math.max(...array)
+        let min = Math.min(...array)
+        let soma = array[0]
+        for (a = 1; a < array.length; a++  ) {
+            soma += array[a]   
+        }
+        res.innerText += `A soma de todos os numeros da lista é ${soma}
+        `
+        res.innerText += `A media dos numeros é ${soma / array.length}
+        `
+        res.innerText += `O maior numero é ${max}
+        `
+        res.innerText += `O menor numero é ${min}`
     }
 }
